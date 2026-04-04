3f76f1d47bdd99e2b0dc19c576e785aa226f19ab
Utsav Thapa <utsav7196@gmail.com>
2026-04-04 16:11:18 +0545

    UserDetails and Security

---
+++ src/main/java/com/example/finance/config/DataInitzializer.java
@@ -0,0 +1,28 @@
+package com.example.finance.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.example.finance.model.entity.User;
+import com.example.finance.repository.UserRepository;
+
+@Configuration
+public class DataInitzializer {
+    @Bean
+    @SuppressWarnings("null")
+    public CommandLineRunner initData(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        return args -> {
+            if (userRepository.findByUsername("admin").isEmpty()) {
+                User admin = User.builder()
+                        .username("admin")
+                        .password(passwordEncoder.encode("admin123")) // Encrypts it!
+                        .role("ROLE_ADMIN")
+                        .build();
+                userRepository.save(admin);
+                System.out.println("Admin user created: admin / admin123");
+            }
+        };
+    }
+}
---
+++ src/main/java/com/example/finance/service/UserDetailService.java
@@ -0,0 +1,30 @@
+package com.example.finance.service;
+
+import com.example.finance.model.entity.User;
+import com.example.finance.repository.UserRepository;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole().replace("ROLE_", ""))
+                .build();
+    }
+}
